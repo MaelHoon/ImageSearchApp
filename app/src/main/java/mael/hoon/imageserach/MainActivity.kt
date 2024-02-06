bbaa0ad3168c510f784bfb3d53ed3f96b56a3fcd
MaelHoon <junghun@iuplusinc.co.kr>
2024-02-06 20:06:46 +0900

    이미지 검색 기능 구현 이미지 갱신 기능 구현

--- app/src/main/java/mael/hoon/imageserach/MainActivity.kt
+++ app/src/main/java/mael/hoon/imageserach/MainActivity.kt
@@ -1,7 +1,12 @@
 package mael.hoon.imageserach
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
@@ -12,14 +17,15 @@ import retrofit2.http.Query
 class MainActivity : AppCompatActivity() {
 
     private val scope = MainScope()
-    private lateinit var binding : ActivityMainBinding
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
 
+        initViews()
+        bindViews()
         fetchRandomPhotos()
     }
 
@@ -28,9 +34,38 @@ class MainActivity : AppCompatActivity() {
         scope.cancel()
     }
 
+    private fun initViews() {
+        binding.recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        binding.recyclerView.adapter = PhotoAdapter()
+    }
+
+    private fun bindViews() {
+        binding.searchEditText.setOnEditorActionListener { editText, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                currentFocus?.let { view ->
+                    val inputMethodManager =
+                        getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+                    inputMethodManager?.hideSoftInputFromWindow(view.windowToken,0)
+
+                    view.clearFocus()
+                }
+                fetchRandomPhotos(editText.text.toString())
+            }
+
+            true
+        }
+        binding.refreshLayout.setOnRefreshListener {
+            fetchRandomPhotos(binding.searchEditText.text.toString())
+        }
+    }
+
     private fun fetchRandomPhotos(query: String? = null) = scope.launch {
-        Repository.getRandomPhotos(query)?.let{photos->
-            photos
+        Repository.getRandomPhotos(query)?.let { photos ->
+            (binding.recyclerView.adapter as? PhotoAdapter)?.apply {
+                this.photos = photos
+                notifyDataSetChanged()
+            }
+            binding.refreshLayout.isRefreshing = false
         }
     }
 }
---
+++ app/src/main/java/mael/hoon/imageserach/data/models/ProfileImageUrls.kt
@@ -0,0 +1,12 @@
+package mael.hoon.imageserach.data.models
+
+import com.google.gson.annotations.SerializedName
+
+data class ProfileImageUrls(
+    @SerializedName("large")
+    val large: String? = null,
+    @SerializedName("medium")
+    val medium : String?=null,
+    @SerializedName("small")
+    val small : String?=null
+)
